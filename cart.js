99f279c977b4c2d44f25ab75a2ccddf143b7fa92
Trent <mr.peschke@gmail.com>
2018-02-15 14:19:45 -0700

    finished

--- cart.js
+++ cart.js
@@ -1,18 +1,21 @@
 const cars = require('./data/cars');
 
 module.exports = {
-  cart: null,
-  total: null,
+  cart: [],
+  total: 0,
 
-  addToCart: function() {
-
+  addToCart: function(i) {
+    this.total = this.total + i['price']
+    this.cart.push(i)
   },
 
-  removeFromCart: function() {
-
+  removeFromCart: function(i, price) {
+    this.total = this.total - price
+    this.cart.splice(1,1)
   },
   
   checkout: function() {
-
+    this.cart = []
+    this.total = 0
   }
 };
